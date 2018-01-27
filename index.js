1385c1f2d67dd3d5d5d486431e9d7b092a73d497
Bruno Sabino <bruno.p.sabino@gmail.com>
2018-01-27 10:08:09 -0200

    index.js do express

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const app = express()
 
-app.get("/hello", (req, res) => {
-  console.log("hello from the other side!")
-  res.send("Hello, world!!!")
+app.get("/hello/:id", (req, res) => {
+  console.log(req.params)
+  res.send(`Hello, ${req.query.name}!!!, id=${req.params.id}`)
 })
 
 app.listen(3000)
